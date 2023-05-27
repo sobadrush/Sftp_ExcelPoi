749d17ed75d4c742db4fd2402afaa5565375971c
Roger Lo <sobadrush@icloud.com>
2023-05-27 23:54:54 +0800

    加入 sftp dependency: jsch

---
+++ src/main/java/com/nanshan/sftp_excelpoi/SftpExcelPoiApplication.java
@@ -0,0 +1,13 @@
+package com.nanshan.sftp_excelpoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpExcelPoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SftpExcelPoiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nanshan/sftp_excelpoi/SftpExcelPoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanshan.sftp_excelpoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftpExcelPoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
